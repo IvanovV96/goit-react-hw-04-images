9e876d0bd340408336832b5c10d4c077104bda1c
Vitaliy Ivanov <dotaivanov@gmail.com>
2022-10-22 21:49:48 +0300

    fix

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+
+class Searchbar extends Component {
+  state = {
+    inputValue: '',
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    const { inputValue } = this.state;
+
+    if (this.state.inputValue.trim() === '') {
+      toast.error('Enter something to search field.');
+      return;
+    }
+    onSubmit(inputValue);
+    this.setState({ inputValue: '' });
+  };
+  handleChange = e => {
+    this.setState({ inputValue: e.currentTarget.value });
+  };
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={inputValue}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
